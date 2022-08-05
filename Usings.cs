819ba69f969178b02a57aa83a413c286128d0859
Benjamin <benjaminjcharlton@gmail.com>
2022-08-05 22:48:15 +0100

    Removed unusued usings declarations.

--- Usings.cs
+++ Usings.cs
@@ -1,6 +1,3 @@
 ﻿global using FluentValidation;
 global using FluentValidation.Results;
 global using GreenPipes;
-global using MassTransit;
-global using FluentValidationForMassTransit;
-global using GreenPipes.Payloads;
